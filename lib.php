c9f8a584eb68b765c0438bec4b2ad81fc3d68d13
Henrique <113316273+HenriquePaini@users.noreply.github.com>
2024-09-08 18:14:10 -0300

    Update lib.php

--- lib.php
+++ lib.php
@@ -5,33 +5,33 @@
 */
 
 function ret_dados_pacien($connTemp) {
-    $meu_array = array();  // Declarando a variável do tipo array
+    $vetor = array();  // Declarando a variável do tipo array
 
     // Abrir o arquivo CSV para leitura
-    $file = fopen('pacientes.csv', 'r');
+    $arquivo = fopen('pacientes.csv', 'r');
 
-    if ($file === false) {
+    if ($arquivo === false) {
         die('Não foi possível abrir o arquivo pacientes.');
     }
 
     // Ler a primeira linha para obter os nomes das colunas
-    $columns = fgetcsv($file, 0, ";");
+    $coluna = fgetcsv($arquivo, 0, ";");
 
-    if ($columns === false) {
-        fclose($file);
+    if ($coluna === false) {
+        fclose($arquivo);
         die('Não foi possível ler a linha de cabeçalhos.');
     }
 
     // Ler as linhas restantes
-    while (($linha = fgetcsv($file, 0, ";")) !== false) {
-        $row = array();
-        foreach ($columns as $index => $column) {
-            $row[$column] = isset($linha[$index]) ? $linha[$index] : '';
+    while (($linha = fgetcsv($arquivo, 0, ";")) !== false) {
+        $vet = array();
+        foreach ($coluna as $i => $column) {
+            $vet[$column] = isset($linha[$i]) ? $linha[$i] : '';
         }
-        $meu_array[] = $row;
+        $vetor[] = $vet;
     }
 
-    fclose($file);
+    fclose($arquivo);
 
     // Preparar a consulta SQL para inserir dados
 
@@ -45,18 +45,18 @@ function ret_dados_pacien($connTemp) {
                 nome=VALUES(nome), sexo=VALUES(sexo), nascimento=VALUES(nascimento), cpf=VALUES(cpf), rg=VALUES(rg), id_convenio=VALUES(id_convenio)";
 
 
-    $stmt_1 = $connTemp->prepare($sql_1);
-    $stmt_2 = $connTemp->prepare($sql_2);
+    $insert_1 = $connTemp->prepare($sql_1);
+    $insert_2 = $connTemp->prepare($sql_2);
 
-    if (!$stmt_1) {
+    if (!$insert_1) {
         die("Erro ao preparar a consulta de convenios: " . $connTemp->error);
     }
 
-    if (!$stmt_2) {
+    if (!$insert_2) {
         die("Erro ao preparar a consulta de pacientes: " . $connTemp->error);
     }
 
-    foreach ($meu_array as $linha) {
+    foreach ($vetor as $linha) {
         // Converter 'sexo_pac' para 'Masculino' ou 'Feminino'
         $sexo = strtoupper(substr($linha['sexo_pac'], 0, 1));  // Pega a primeira letra e converte para maiúscula
         if ($sexo === 'M') {
@@ -77,7 +77,7 @@ function ret_dados_pacien($connTemp) {
         $nascimento_formatado = $nascimento->format('Y-m-d');
 
         // Bind dos parâmetros para a tabela convenios
-        $stmt_1->bind_param(
+        $insert_1->bind_param(
             'iss',
             $linha['id_conv'],
             $linha['convenio'],
@@ -85,13 +85,13 @@ function ret_dados_pacien($connTemp) {
         );
 
         // Executar inserção na tabela convenios
-        if (!$stmt_1->execute()) {
-            echo "Erro ao inserir os dados do convênio do paciente " . $linha['nome_paciente'] . ": " . $stmt_1->error . "<br>";
+        if (!$insert_1->execute()) {
+            echo "Erro ao inserir os dados do convênio do paciente " . $linha['nome_paciente'] . ": " . $insert_1->error . "<br>";
             continue;  // Pular para o próximo se falhar na inserção de convênios
         }
 
         // Bind dos parâmetros para a tabela pacientes
-        $stmt_2->bind_param(
+        $insert_2->bind_param(
             'isssssi',
             $linha['cod_paciente'],
             $linha['nome_paciente'],
@@ -103,43 +103,43 @@ function ret_dados_pacien($connTemp) {
         );
 
         // Executar inserção na tabela pacientes
-        if (!$stmt_2->execute()) {
-            echo "Erro ao inserir os dados do paciente " . $linha['nome_paciente'] . ": " . $stmt_2->error . "<br>";
+        if (!$insert_2->execute()) {
+            echo "Erro ao inserir os dados do paciente " . $linha['nome_paciente'] . ": " . $insert_2->error . "<br>";
         } 
     }
 
-    $stmt_1->close();
-    $stmt_2->close();
+    $insert_1->close();
+    $insert_2->close();
 };
 
 /*-------------------------*/
 
 function ret_dados_agend($connTemp){
     // Abrir o arquivo CSV para leitura
-    $file = fopen('agendamentos.csv', 'r');
+    $arquivo = fopen('agendamentos.csv', 'r');
 
-    if ($file === false) {
+    if ($arquivo === false) {
         die('Não foi possível abrir o arquivo agendamentos.');
     }
 
     // Ler a primeira linha para obter os nomes das colunas
-    $columns = fgetcsv($file, 0, ";");
+    $coluna = fgetcsv($arquivo, 0, ";");
 
-    if ($columns === false) {
-        fclose($file);
+    if ($coluna === false) {
+        fclose($arquivo);
         die('Não foi possível ler a linha de cabeçalhos.');
     }
 
     // Ler as linhas restantes
-    while (($linha = fgetcsv($file, 0, ";")) !== false) {
-        $row = array();
-        foreach ($columns as $index => $column) {
-            $row[$column] = isset($linha[$index]) ? $linha[$index] : '';
+    while (($linha = fgetcsv($arquivo, 0, ";")) !== false) {
+        $vet = array();
+        foreach ($coluna as $i => $column) {
+            $vet[$column] = isset($linha[$i]) ? $linha[$i] : '';
         }
-        $meu_array[] = $row;
+        $vetor[] = $vet;
     }
 
-    fclose($file);
+    fclose($arquivo);
 
     // Preparar a consulta SQL para inserir dados
 
@@ -155,25 +155,25 @@ function ret_dados_agend($connTemp){
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)
                 ON DUPLICATE KEY UPDATE id_convenio=VALUES(id_convenio), id_procedimento=VALUES(id_procedimento), observacoes=VALUES(observacoes)";
 
-    $stmt_1 = $connTemp->prepare($sql_1);
-    $stmt_2 = $connTemp->prepare($sql_2);
-    $stmt_3 = $connTemp->prepare($sql_3);
+    $insert_1 = $connTemp->prepare($sql_1);
+    $insert_2 = $connTemp->prepare($sql_2);
+    $insert_3 = $connTemp->prepare($sql_3);
 
 
 
-    if (!$stmt_1) {
+    if (!$insert_1) {
         die("Erro ao preparar a consulta de convenios: " . $connTemp->error);
     }
 
-    if (!$stmt_2) {
+    if (!$insert_2) {
         die("Erro ao preparar a consulta de pacientes: " . $connTemp->error);
     }
 
-    if (!$stmt_3) {
+    if (!$insert_3) {
         die("Erro ao preparar a consulta de pacientes: " . $connTemp->error);
     }
 
-    foreach ($meu_array as $linha) {
+    foreach ($vetor as $linha) {
 
         // Converter a data de data do formato dd/mm/yyyy para yyyy-mm-dd hh24:mi:ss
         $data_inicio = $linha['dia'] . ' ' . $linha['hora_inicio'];
@@ -195,14 +195,14 @@ function ret_dados_agend($connTemp){
         $vd_data_fim = $dh_fim->format('Y-m-d H:i:s');
 
         // Bind dos parâmetros para a tabela procedimentos
-        $stmt_1->bind_param(
+        $insert_1->bind_param(
             'ss',
             $linha['procedimento'],
             $linha['descricao']
         );
 
-        if (!$stmt_1->execute()) {
-            echo "Erro ao inserir os dados do procedimento " . $linha['procedimento'] . ": " . $stmt_1->error . "<br>";
+        if (!$insert_1->execute()) {
+            echo "Erro ao inserir os dados do procedimento " . $linha['procedimento'] . ": " . $insert_1->error . "<br>";
             continue;  // Pular para o próximo se falhar na inserção de procedimento
         }
 
@@ -210,18 +210,18 @@ function ret_dados_agend($connTemp){
         $procedimento_id = $connTemp->insert_id;
 
         // Bind dos parâmetros para a tabela profissionais
-        $stmt_2->bind_param(
+        $insert_2->bind_param(
             'is',
             $linha['cod_medico'],
             $linha['medico']
         );
-        if (!$stmt_2->execute()) {
-            echo "Erro ao inserir os dados do profissional " . $linha['nome'] . ": " . $stmt_2->error . "<br>";
+        if (!$insert_2->execute()) {
+            echo "Erro ao inserir os dados do profissional " . $linha['nome'] . ": " . $insert_2->error . "<br>";
             continue;  // Pular para o próximo se falhar na inserção de profissionais
         }
 
         // Bind dos parâmetros para a tabela agendamentos
-        $stmt_3->bind_param(
+        $insert_3->bind_param(
             'iiissiis',
             $linha['cod_agendamento'],
             $linha['cod_paciente'],
@@ -232,16 +232,16 @@ function ret_dados_agend($connTemp){
             $procedimento_id,
             $linha['observacoes']
         );
-        if (!$stmt_3->execute()) {
-            echo "Erro ao inserir os dados do agendamento " . $linha['id'] . ": " . $stmt_3->error . "<br>";
+        if (!$insert_3->execute()) {
+            echo "Erro ao inserir os dados do agendamento " . $linha['id'] . ": " . $insert_3->error . "<br>";
             continue;  // Pular para o próximo se falhar na inserção de agendamentos
         }
 
     }
 
-    $stmt_1->close();
-    $stmt_2->close();
-    $stmt_3->close();
+    $insert_1->close();
+    $insert_2->close();
+    $insert_3->close();
 };
 
 function dateNow(){
